a1cda2cdd6d4b2955335ef3f86c1a516c9b15369
adwait <apkharche@gmail.com>
2019-10-16 18:00:18 +0530

    arithmetic.py

---
+++ python/arithmetic.py
@@ -0,0 +1,28 @@
+print("Adwait Kharche")
+
+def averageNumbers(a,b):
+    average = 0
+    for i in range(a,b+1):
+        print('Sum of %d + %d =' % (average,i))
+        average = average + i
+        print('%d' % (average))
+    return average
+
+def multiplyNumbers(a,b):
+    return a*b
+
+print("Multiplication of numbers:")
+print(multiplyNumbers(50,40))
+
+print("Multiplication of numbers:")
+print(multiplyNumbers(5,20))
+
+avg = averageNumbers(1,20)
+print('Average of numnbers %d-%d = %d' % (1,20,avg))
+
+avg = averageNumbers(1,5)
+print('Average of numnbers %d-%d = %d' % (1,5,avg))
+
+print('Adwait Kharche')
+print("Adwait Kharche")
+
